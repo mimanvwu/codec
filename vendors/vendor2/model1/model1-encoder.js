bc57b934a6ac56e2718adfe11c8f608cc0ce8133
Youny <youny@milesight.com>
2025-04-24 19:56:44 +0800

    update codec

---
+++ vendors/vendor2/model1/model1-encoder.js
@@ -0,0 +1,11 @@
+/**
+ * Payload Encoder
+ *
+ * Copyright 2025 Milesight IoT
+ *
+ * @product AM308L
+ */
+function Encode(fPort, obj) {
+    var encoded = [];
+    return encoded;
+}
---
+++ vendors/vendor2/model2/model2-encoder.js
@@ -0,0 +1,11 @@
+/**
+ * Payload Encoder
+ *
+ * Copyright 2025 Milesight IoT
+ *
+ * @product AT101
+ */
+function Encode(fPort, obj) {
+    var encoded = [];
+    return encoded;
+}
